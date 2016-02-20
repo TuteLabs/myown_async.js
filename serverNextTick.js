a94a126f4c0a213afa32677ef99d07dac15110de
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-02-20 16:49:54 +0530

    Add:Step 4: Execute time consuming function

--- serverNextTick.js
+++ serverNextTick.js
@@ -10,5 +10,8 @@ function compute() {
 	console.log("compute finished")
 }
 
-console.log("I am going to be busy for some time...wait for me please")
+// console.log("I am going to be busy for some time...wait for me please")
 
+console.log("Busy Executing a task...")
+compute()
+console.log("I am Free now :)")
